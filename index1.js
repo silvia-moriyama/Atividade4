e86c9ef19ddabb366060e56140c0d5dc3aae62d5
silvia-moriyama <143272395+silvia-moriyama@users.noreply.github.com>
2023-09-12 07:00:10 -0300

    Update index1
    Exercicio 1

--- index1.js
+++ index1.js
@@ -1,1 +1,17 @@
+function maior(num1,num2,num3){
+
+const maior = Math.max(num1, num2, num3);
+
+console.log("O maior numero e " + maior);
+
+}
+
+
+
 const prompt = require("prompt-sync")();
+
+const num1 = parseFloat(prompt("Digite o primeiro numero: "));
+const num2 = parseFloat(prompt("Digite o segundo numero: "));
+const num3 = parseFloat(prompt("Digite o terceiro numero: "));
+
+maior(num1,num2,num3);
